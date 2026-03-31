1d0b5b39febc8a1a6dc82d06cb455e103bf58a8f
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-03-31 17:25:57 +0700

    perulangan w for

---
+++ loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    int x;
+
+    // perulangan dengan for
+    // mencetak 5 kali
+    cout << "PERULANGAN FOR" << endl for (int i = 0; i <= 4; i++)
+    {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
+}
